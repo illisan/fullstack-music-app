cb34465cd41e406302f1535a8c558e463add7f59
illisan <illiv.sandra@gmail.com>
2018-03-04 14:46:45 -0800

    updates

--- front_end/src/components/App/SongsList/SongsList.jsx
+++ front_end/src/components/App/SongsList/SongsList.jsx
@@ -11,10 +11,13 @@ class SongsList extends Component {
         let songsJSX = songsArr.map((song, i) => {
             console.log(song)
             return <div key={song.id}>
+            <div className="containter">
                 <div className="songContent">
                     <h2 className="sonListTitle"><Link to={`/${song.id}`}>{song.title}</Link></h2>
                     <button onClick={() => { this.props.playAudio(song.id) }}>Play</button>
                 </div>
+                </div>
+
             </div>
         })
 
